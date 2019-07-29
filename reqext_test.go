0a2a8ec96e177bcc140b86a4328c21b962c2caa7
bingoohuang <bingoo.huang@gmail.com>
2019-07-29 14:30:44 +0800

    Create reqext_test.go

---
+++ reqext_test.go
@@ -0,0 +1,21 @@
+package gonet_test
+
+import (
+	"testing"
+
+	"github.com/bingoohuang/gonet"
+	"github.com/stretchr/testify/assert"
+)
+
+type GetRsp struct {
+	Origin string `json:"origin"`
+	Url    string `json:"url"`
+}
+
+func TestRestGet(t *testing.T) {
+	var rsp GetRsp
+	url := `https://httpbin.org/get`
+	err := gonet.RestGet(url, &rsp)
+	assert.Nil(t, err)
+	assert.Equal(t, url, rsp.Url)
+}
